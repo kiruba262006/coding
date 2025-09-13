d373a59bb48dde4984121ac18fdb3a5e1b814a85
Kiruba T <tj.kiruba006@gmail.com>
2025-09-13 19:26:46 +0530

    Add files via upload

---
+++ mink.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class mink{
+public static void main (String[] args){
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int arr[]=new int [n];
+  int sum=0;
+for (int i=0;i<n;i++){
+    arr[i]=sc.nextInt();
+    }
+    int min=Integer.MAX_VALUE;
+   for( int i=0;i<n;i++){
+   if(min>arr[i]){
+    min=arr[i];
+    }
+   }
+   System.out.println("min value ="+min);
+}
+}
