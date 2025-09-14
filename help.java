de3be2e155634344616930897310a6a06044604c
Kiruba T <tj.kiruba006@gmail.com>
2025-09-14 18:30:18 +0530

    Add files via upload

---
+++ help.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class help{
+    public static void main(String[] args){
+             for(int i=1;i<6;i++){
+          for(int j=1;j<6+1-i;j++){
+        System.out.print("* ");
+    }
+    System.out.println();
+    }
+  }
+}
