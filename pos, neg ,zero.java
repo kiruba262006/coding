dbeceaf1c77152d3ef947cd34cb2f954770b8c55
Kiruba T <tj.kiruba006@gmail.com>
2025-09-17 22:14:42 +0530

    pos, neg ,zero.java

---
+++ pos, neg ,zero.java
@@ -0,0 +1,15 @@
+  import java.util.Scanner;
+public class Q8_PosNegZero {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        if (num > 0) {
+            System.out.println(num + " is positive");
+        } else if (num < 0) {
+            System.out.println(num + " is negative");
+        } else {
+            System.out.println(num + " is zero");
+        }
+        sc.close();
+    }
+}
