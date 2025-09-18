d19679a279f81c6f695fa136ab0cd12e6cccfdb4
Kiruba T <tj.kiruba006@gmail.com>
2025-09-18 22:43:06 +0530

    armstr.java

---
+++ armstr.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class armstrong {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int original = num;
+        int sum = 0;
+        int digits = String.valueOf(num).length();
+        while (num > 0) {
+            int digit = num % 10;
+            sum += Math.pow(digit, digits);
+            num /= 10;
+        }
+        if (sum == original) {
+            System.out.println("Armstrong Number");
+        } else {
+            System.out.println("Not an Armstrong Number");
+        }
+        sc.close();
+    }
+}
