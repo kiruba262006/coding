96eec99b5fa63235c0a8435a1853c5773a9c7f0a
Kiruba T <tj.kiruba006@gmail.com>
2025-09-18 22:08:01 +0530

    product of digits.java

---
+++ product of digits.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class ProductOfDigits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int product = 1;
+        if (num == 0) {
+            System.out.println(0);
+            return;
+        }
+        while (num > 0) {
+            int digit = num % 10;
+            product *= digit;
+            num /= 10;
+        }
+        System.out.println(product);
+        sc.close();
+    }
+}
